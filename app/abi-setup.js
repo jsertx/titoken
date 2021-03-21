444f235dd685929a2dcc87b0bda5383e33f0b210
Sergio <sergiazow@gmail.com>
2021-03-21 20:10:26 +0100

    feat: soft stop of abi build

--- app/abi-setup.js
+++ app/abi-setup.js
@@ -4,8 +4,7 @@ const contractAbiFilePath = "../abi/__contracts_Ballot_sol_Ballot.abi";
 if(!fs.existsSync(contractAbiFilePath)){
     console.log("abi file not found at", contractAbiFilePath)
     console.log("please run 'npm run build:abi' at the root of the project")
-    process.exit();
+} else {
+    const abi = fs.readFileSync(contractAbiFilePath)
+    fs.writeFileSync("./lib/contract/abi.js", `export default ${abi}`);
 }
-
-const abi = fs.readFileSync(contractAbiFilePath)
-fs.writeFileSync("./lib/contract/abi.js", `export default ${abi}`);
